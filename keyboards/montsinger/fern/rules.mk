efc531b24fcae253c4ef54b158a2bc584e6c1db6
Ross <rmontsinger@gmail.com>
2023-02-26 00:12:42 -0500

    fern 002 up and running with avr

--- keyboards/montsinger/fern/rules.mk
+++ keyboards/montsinger/fern/rules.mk
@@ -1,4 +1,8 @@
-# Build Options
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = atmel-dfu# Build Options
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
