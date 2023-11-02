f2c5dfde223a706ee6661f235ba3095b3d13947b
NCKiser <noahkiser95mygc@gmail.com>
2023-11-02 13:42:24 -0500

    vertical, scroll speed, and press-hold

--- keyboards/kiserdesigns/trackpad/keymaps/default/keymap.c
+++ keyboards/kiserdesigns/trackpad/keymaps/default/keymap.c
@@ -20,3 +20,63 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,   KC_W
     )
 };
+
+#define SCROLL_AVERAGE 10
+report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
+    static int16_t h       = 0;
+    static uint8_t h_count = 0;
+    static int16_t v       = 0;
+    static uint8_t v_count = 0;
+
+    if (mouse_report.h != 0) {
+        h_count++;
+        h += mouse_report.h;
+        mouse_report.h = 0;
+        if (h_count == SCROLL_AVERAGE) {
+            h_count = 0;
+            if (h < 0) {
+                mouse_report.h = -1;
+            } else {
+                mouse_report.h = 1;
+            }
+            h = 0;
+        }
+    } else {
+        if (h_count) {
+            if (h < 0) {
+                mouse_report.h = -1;
+            } else {
+                mouse_report.h = 1;
+            }
+        }
+        h_count = 0;
+        h       = 0;
+    }
+
+    if (mouse_report.v != 0) {
+        v_count++;
+        v += mouse_report.v;
+        mouse_report.v = 0;
+        if (v_count == SCROLL_AVERAGE) {
+            v_count = 0;
+            if (v < 0) {
+                mouse_report.v = -1;
+            } else {
+                mouse_report.v = 1;
+            }
+            v = 0;
+        }
+    } else {
+        if (v_count) {
+            if (v < 0) {
+                mouse_report.v = -1;
+            } else {
+                mouse_report.v = 1;
+            }
+        }
+        v_count = 0;
+        v       = 0;
+    }
+
+    return mouse_report;
+}
